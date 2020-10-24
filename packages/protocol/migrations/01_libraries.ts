145a5bbf85da89911e0d01422e5b78f1170a02e6
Asa Oines <a@celo.org>
2020-10-23 20:07:31 -0400

    WIP

--- packages/protocol/migrations/01_libraries.ts
+++ packages/protocol/migrations/01_libraries.ts
@@ -1,10 +1,24 @@
 import { linkedLibraries } from '@celo/protocol/migrationsConfig'
+import { ProxyInstance } from 'types'
 
 module.exports = (deployer: any) => {
   Object.keys(linkedLibraries).forEach((lib: string) => {
+    const LibraryProxy = artifacts.require(lib + 'Proxy')
     const Library = artifacts.require(lib)
+    console.log('Deploying proxy for', lib)
+    deployer.deploy(LibraryProxy)
+    console.log('Deploying', lib)
     deployer.deploy(Library)
-    const Contracts = linkedLibraries[lib].map((contract: string) => artifacts.require(contract))
-    deployer.link(Library, Contracts)
+    deployer.then(async () => {
+      const library = await Library.deployed()
+      const proxy: ProxyInstance = await LibraryProxy.deployed()
+      console.log('Linking proxy to', lib)
+      await proxy._setImplementation(library.address)
+      const Contracts = linkedLibraries[lib].map((contract: string) => artifacts.require(contract))
+      const ProxiedLibrary = Library.at(proxy.address)
+      console.log('Linking', lib, proxy.address, 'to', linkedLibraries[lib])
+      Contracts.map((c: any) => c.link(ProxiedLibrary))
+      deployer.link(ProxiedLibrary, Contracts)
+    })
   })
 }
